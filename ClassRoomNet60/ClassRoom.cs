9676eaa7d8bfbdc5cb1655692331f7aaf1e07e94
btrhazara@gmail.com <btrhazara@gmail.com>
2025-02-06 13:30:07 +0100

    :(

---
+++ ClassRoomNet60/ClassRoom.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    internal class ClassRoom
+    {
+        public string ClassName { get; set; }
+        List<student> studentList; 
+        DateTime SemesterStart ;
+
+
+        public ClassRoom ()
+    }
+}
---
+++ ClassRoomNet60/student.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassRoomNet60
+{
+    internal class student
+    {
+        public string Name { get; set; }
+        public int BirthMonth {  get; set; }
+        public int Birthday { get; set; }
+
+        public student(string name, int birthmonth, int birthday) 
+        {
+            Name = name;
+            BirthMonth = birthmonth;
+            Birthday = birthday;
+            
+
+
+        }
+}
+}
+
